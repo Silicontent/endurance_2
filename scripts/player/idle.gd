6d0f8172b751bafbb7eedf4041e1f9cb9fbd4183
Silicontent <61814046+Silicontent@users.noreply.github.com>
2023-05-18 15:30:24 -0500

    first enemy pt.1

--- scripts/player/idle.gd
+++ scripts/player/idle.gd
@@ -11,6 +11,14 @@ func enter(_msg := {}) -> void:
 	player.velocity = Vector2.ZERO
 
 
+# allows dash to be activated even while not moving
+func handle_input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		if event.pressed and event.keycode == KEY_SPACE:
+			if player.can_dash:
+				state_machine.transition_to("Dash")
+
+
 func physics_update(_delta: float) -> void:
 	for input in move_inputs:
 		if Input.is_action_pressed(input):
